47aed04b98d4706f14eadd64950b47a20310809a
colobas <mail@gpir.es>
2019-06-12 18:08:19 -0700

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='normalizing-flows',
+      version='0.0',
+      description='Guilherme Pires MSc Thesis',
+      author='Guilherme Pires',
+      author_email='mail@gpir.es',
+      packages=['normalizing_flows'],
+      zip_safe=False)
