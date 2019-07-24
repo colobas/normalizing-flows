48f23b0e3dd12a83eb0502cfd747c8fd393043ea
colobas <mail@gpir.es>
2019-07-24 09:56:43 -0700

    coupling layer implemented not tested

--- normalizing_flows/flows/coupling.py
+++ normalizing_flows/flows/coupling.py
@@ -21,24 +21,25 @@ class CouplingLayerFlow(Flow):
         self.x2_mask = make_mask_func(x2_mask)
 
     def _call(self, z):
-        # note that y1 = x1
-        y1 = self.x1_mask(z)
+        # note that z2 = x2
+        z1 = self.x1_mask(z)
         x2 = self.x2_mask(z)
 
-        y2 = x2 * (self.s(y1).exp()) + self.t(y1)
-
-        return torch.stack([y1, y2])
-
-    def _inverse(self, z):
-        # note that y1 = x1
-        x1 = self.x1_mask(z)
-        y2 = self.x2_mask(z)
-
-        x2 = (y2 - self.t(x1)) * (1/(self.s(y1).exp()))
+        x1 = z1 * (self.s(z2).exp()) + self.t(z2)
 
         return torch.stack([x1, x2])
 
+    def _inverse(self, x):
+        # note that z2 = x2
+        x1 = self.x1_mask(x)
+        z2 = self.x2_mask(x)
 
+        z1 = (x1 - self.t(z2)) * ((-self.s(z2).exp()))
+
+        return torch.stack([x1, x2])
 
     def log_abs_det_jacobian(self, z, z_next):
+        z2 = self.x2_mask(z)
+
+        return self.s(z2).sum(dim=1).abs()
 
