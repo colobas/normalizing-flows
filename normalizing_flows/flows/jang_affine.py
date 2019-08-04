355f13db5aaa1e17c2eddce01d7d65e2d281fc78
colobas <mail@gpir.es>
2019-08-03 19:28:26 -0700

    half moon example working

--- normalizing_flows/flows/jang_affine.py
+++ normalizing_flows/flows/jang_affine.py
@@ -15,41 +15,45 @@ else:
 class StructuredAffineFlow(Flow):
     def __init__(self, dim):
         super(StructuredAffineFlow, self).__init__()
-        self.V = nn.Parameter(torch.randn(dim, dim))
-        nn.init.xavier_uniform(self.V)
 
-        shift = nn.init.xavier_uniform(torch.randn(dim, 1)).squeeze()
+        self.V = nn.Parameter(torch.rand(dim, dim))
+        nn.init.xavier_uniform_(self.V)
+
+        shift = nn.init.xavier_uniform_(torch.rand(dim, 1)).squeeze()
         self.shift = nn.Parameter(shift)
-
-
 
         self.tril_mask = torch.zeros(dim, dim)
         self.tril_indices = np.tril_indices(dim)
         self.tril_mask[self.tril_indices] = 1.
 
         L = torch.zeros(dim, dim)
-        L[self.tril_indices] = nn.init.xavier_uniform(
+        L[self.tril_indices] = nn.init.xavier_uniform_(
                 torch.ones(len(self.tril_indices[0]), 1)).squeeze()
         self.L = nn.Parameter(L)
 
         self.I = torch.eye(dim)
 
+        if dim == 1:
+            self.mul = torch.mul
+        else:
+            self.mul = torch.matmul
+
     @property
     def weights(self):
-        return (self.L * self.tril_mask) + (self.V @ self.I @ self.V.t())
+        return (self.L * self.tril_mask) + self.mul(self.V, self.V.t())
 
     def _call(self, z):
-        return (self.weights @ z.unsqueeze(-1)).squeeze(-1) + self.shift
+        return self.mul(self.weights, z.unsqueeze(-1)).squeeze(-1) + self.shift
 
     def _inverse(self, z):
-        return (torch.inverse(self.weights) @ (z - self.shift).unsqueeze(-1)).squeeze(-1)
+        return self.mul(torch.inverse(self.weights), (z - self.shift).unsqueeze(-1)).squeeze(-1)
 
     def log_abs_det_jacobian(self, z, z_next):
         """
         roughly following tensorflow's LinearOperatorLowRankUpdate logic and
         naming, but U and V are the same, and D is the identity
         """
-        log_det_L = self.L.diag().prod().abs().log()
+        log_det_L = self.L.diag().abs().log().sum()
 
         linv_u = triangular_solve(self.V, self.L * self.tril_mask, upper=False)[0]
         vt_linv_u = self.V.t() @ linv_u
