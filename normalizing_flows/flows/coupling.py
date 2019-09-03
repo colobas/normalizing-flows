b22a79c48e6eb8b1f40362dfc044bda236b7a5c5
colobas <mail@gpir.es>
2019-09-03 02:10:53 -0700

    coupling layer working

--- normalizing_flows/flows/coupling.py
+++ normalizing_flows/flows/coupling.py
@@ -10,7 +10,7 @@ class CouplingLayerFlow(Flow):
     def __init__(self, dim, hidden_size, n_hidden, mask):
         super().__init__()
 
-        self.register_buffer('mask', torch.LongTensor(mask))
+        self.register_buffer('mask', mask.float())
 
         self.x1_mask = lambda z: torch.gather(z, 1, torch.nonzero(mask).repeat((len(z),1)))
         self.x2_mask = lambda z: torch.gather(z, 1, torch.nonzero(1-mask).repeat((len(z),1)))
@@ -30,27 +30,28 @@ class CouplingLayerFlow(Flow):
         self.t = nn.Sequential(*t)
 
     def forward(self, z):
-        # note that z2 = x2
         z1 = self.x1_mask(z)
-        x2 = self.x2_mask(z)
+        z2 = self.x2_mask(z)
 
-        x1 = z1 * (self.s(x2).exp()) + self.t(x2)
+        x1 = (z1 - self.t(z2)) * torch.exp(-self.s(z2))
 
-        return torch.cat([x1, x2], dim=1)
+        # note that z2 = x2
+        return torch.cat((x1, z2), dim=1)
 
     def inverse(self, x):
-        # note that z2 = x2
         x1 = self.x1_mask(x)
-        z2 = self.x2_mask(x)
+        x2 = self.x2_mask(x)
 
-        z1 = (x1 - self.t(z2)) * ((-self.s(z2)).exp())
+        z1 = x1 * torch.exp(self.s(x2)) + self.t(x2)
 
-        return torch.cat([z1, z2], dim=1)
+        # note that x2 = z2
+        return torch.cat((z1, x2), dim=1)
 
     def log_abs_det_jacobian(self, z, z_next=None):
-        z2 = self.x2_mask(z)
-        return self.s(z2).sum(dim=1)
+        #z2 = (self.x2_mask(z) if z_next is None else self.x2_mask(z_next))
+        z2 = self.x2_mask(z_next)
+        return -self.s(z2).sum(dim=1)
 
-    def to(self, device="cuda:0"):
+    def to(self):
         super(CouplingLayerFlow, self).to(device)
 
